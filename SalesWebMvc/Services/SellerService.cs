125b2a69a7024a51971d0ebb54621bb1cf3fea5b
Danilo A. Santos <danilo_as1992@hotmail.com>
2021-04-07 22:49:17 -0300

    add sellerService and findAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Service
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using SalesWebMvc.Service;
 
 namespace SalesWebMvc
 {
@@ -35,6 +36,7 @@ namespace SalesWebMvc
                      builder => builder.MigrationsAssembly("SalesWebMvc")));
             
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
